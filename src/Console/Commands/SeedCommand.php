aa65d9920ea6e9fc715c1347b05099015af78b55
misaf <misaf.1990@gmail.com>
2026-07-28 11:39:52 +0000

    docs: add UPGRADING guide and update README files across packages

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -13,10 +13,10 @@ final class SeedCommand extends TenantSeedCommand
     protected const string MODULE_NAME = AuthifyLogPlugin::ID;
 
     protected $signature = self::MODULE_NAME . ':seed
-        {tenant? : Tenant ID or slug to seed activity log data for}
+        {tenant? : Tenant ID or slug to seed authentication log data for}
         {seeders?* : Seeder keys to run. Use "all" or one or more of: permissions}';
 
-    protected $description = 'Seed activity log module data for a tenant';
+    protected $description = 'Seed authentication log module data for a tenant';
 
     /**
      * @return array<string, class-string>
